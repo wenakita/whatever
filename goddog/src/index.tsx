f866637a17e80a8cb3cc021ad39d8694b511810b
exoticsolutions <rafajr1263@gmail.com>
2024-04-10 11:35:04 -0400

    final-frame

--- goddog/src/index.tsx
+++ goddog/src/index.tsx
@@ -1,72 +1,86 @@
-import { serve } from '@hono/node-server'
-import { serveStatic } from '@hono/node-server/serve-static'
-import { Button, Frog, TextInput } from 'frog'
-import { devtools } from 'frog/dev'
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+import { Button, Frog, TextInput } from "frog";
+import { devtools } from "frog/dev";
 // import { neynar } from 'frog/hubs'
 
 export const app = new Frog({
+  basePath: "/goddog",
   // Supply a Hub to enable frame verification.
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
-})
+});
+let currentSlide: number = 1;
+app.use("/*", serveStatic({ root: "./public" }));
 
-app.use('/*', serveStatic({ root: './public' }))
-
-app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
+app.frame("/", (c) => {
+  const { buttonValue } = c;
+  console.log(buttonValue);
   return c.res({
-    image: (
-      <div
-        style={{
-          alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-        <div
-          style={{
-            color: 'white',
-            fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
-        </div>
-      </div>
-    ),
+    image: "http://localhost:5173/slide1.png",
+    action: "/slides",
     intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+      <Button value="previous">⏪</Button>,
+      <Button.Link href="https://t.me/goddogportal">Socials</Button.Link>,
+      <Button.Link href="https://interchain.axelar.dev/base/0xDDf7d080C82b8048BAAe54e376a3406572429b4e">
+        Interchain
+      </Button.Link>,
+      <Button value="next">⏩</Button>,
     ],
-  })
-})
+  });
+});
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+app.frame("/slides", (c) => {
+  const { buttonValue } = c;
+  console.log(buttonValue);
 
-devtools(app, { serveStatic })
+  if (buttonValue == "next") {
+    if (currentSlide !== 5) {
+      ++currentSlide;
+      return c.res({
+        image: `http://localhost:5173/slide${currentSlide}.png`,
+        intents: [
+          <Button value="previous">⏪</Button>,
+          <Button.Link href="https://t.me/goddogportal">Socials</Button.Link>,
+          <Button.Link href="https://interchain.axelar.dev/base/0xDDf7d080C82b8048BAAe54e376a3406572429b4e">
+            Interchain
+          </Button.Link>,
+          <Button value="next">⏩</Button>,
+        ],
+      });
+    } else if (currentSlide === 5) {
+      currentSlide = 1;
+      return c.res({
+        image: `http://localhost:5173/slide${currentSlide}.png`,
+        intents: [
+          <Button value="previous">⏪</Button>,
+          <Button.Link href="https://t.me/goddogportal">Socials</Button.Link>,
+          <Button.Link href="https://interchain.axelar.dev/base/0xDDf7d080C82b8048BAAe54e376a3406572429b4e">
+            Interchain
+          </Button.Link>,
+          <Button value="next">⏩</Button>,
+        ],
+      });
+    }
+  }
+  return c.res({
+    image: "http://localhost:5173/slide1.png",
+    intents: [
+      <Button value="previous">⏪</Button>,
+      <Button.Link href="https://t.me/goddogportal">Socials</Button.Link>,
+      <Button.Link href="https://interchain.axelar.dev/base/0xDDf7d080C82b8048BAAe54e376a3406572429b4e">
+        Interchain
+      </Button.Link>,
+      <Button value="next">⏩</Button>,
+    ],
+  });
+});
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
+
+devtools(app, { serveStatic });
 
 serve({
   fetch: app.fetch,
   port,
-})
+});
