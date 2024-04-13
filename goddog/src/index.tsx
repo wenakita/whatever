d9531e832afafd59b82b0034eeb822382f7e4da5
exoticsolutions <rafajr1263@gmail.com>
2024-04-13 03:41:39 -0400

    fina

--- goddog/src/index.tsx
+++ goddog/src/index.tsx
@@ -16,8 +16,7 @@ import {
 import { serveStatic } from "frog/serve-static";
 import { config } from "dotenv";
 config();
-const port = process.env.PORT || 3000;
-const host = process.env.HOST || "localhost";
+const port = process.env.PORT || 4000;
 export const app = new Frog({
   ui: { vars },
 })
@@ -194,6 +193,5 @@ devtools(app, { serveStatic });
 
 serve({
   fetch: app.fetch,
-  port: port || 3000,
-  hostname: host,
+  port: port,
 });
