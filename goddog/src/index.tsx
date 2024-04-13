2a5dfeaf35c4ef8856534d5ff67c03816a007f34
exoticsolutions <rafajr1263@gmail.com>
2024-04-13 01:34:42 -0400

    final-first

--- goddog/src/index.tsx
+++ goddog/src/index.tsx
@@ -1,128 +1,194 @@
-import { serve } from "@hono/node-server";
 import { Button, Frog } from "frog";
-import { createSystem } from "frog/ui";
+import { devtools } from "frog/dev";
+import { serve } from "@hono/node-server";
+import {
+  Box,
+  Heading,
+  Text,
+  VStack,
+  vars,
+  Rows,
+  Row,
+  Columns,
+  Column,
+} from "./ui.js";
+import { serveStatic } from "frog/serve-static";
 
-const { Image } = createSystem();
-type state = {
-  count: number;
-};
-export const app = new Frog<{ State: state }>({
-  initialState: {
-    count: 4,
-  },
-});
-
-app.frame("/", (c) => {
-  const { buttonValue, status, deriveState } = c;
-
-  const state = deriveState((previousState) => {
-    if (buttonValue === "next") {
-      previousState.count++;
-    } else if (buttonValue === "previous") {
-      previousState.count--;
-    }
-  });
-  console.log(state.count);
-  return c.res({
-    image: (
-      <div
-        style={{
-          color: "white",
-          display: "flex",
-          fontSize: 60,
-          justifyContent: "center",
-        }}
-      >
-        {"🐕 GodDog: The future of memecoins 🐕"}
-      </div>
-    ),
-    action: "/goddog",
-    intents: [
-      <Button value="previous">⏪</Button>,
-      <Button value="banana">Banana</Button>,
-      <Button value="next">⏩</Button>,
-    ],
-  });
-});
-
-app.frame("/goddog", (c) => {
-  const { buttonValue, status, deriveState } = c;
-  const state = deriveState((previousState) => {
-    if (buttonValue === "next") {
-      previousState.count++;
-    } else if (buttonValue === "previous") {
-      previousState.count--;
-    }
-  });
-  console.log(state.count);
-  if (buttonValue === "next") {
+export const app = new Frog({
+  ui: { vars },
+})
+  .frame("/", (c) => {
     return c.res({
       image: (
-        <div
-          style={{
-            color: "white",
-            display: "flex",
-            fontSize: 40,
-          }}
-        >
-          {`
-        
-            GodDog's integration with an omnichain infrastructure ensures interoperability across multiple blockchain networks, enhancing accessibility and utility for users. This approach also bolsters security, scalability, and future adaptability, positioning GodDog as a robust and versatile memecoin within the broader blockchain ecosystem.
-        `}
-        </div>
+        <Rows backgroundColor="background" padding="32">
+          <Row height="3/4" alignVertical="bottom">
+            <Heading>GodDog 🐕</Heading>
+            <Text color="text200" size="20">
+              the future of memecoins
+            </Text>
+          </Row>
+          <Row
+            height="0.5/4"
+            border={"gray"}
+            borderBottom={"none"}
+            borderRight={"none"}
+            borderLeft={"none"}
+            alignVertical="bottom"
+          >
+            <Text size={"15"}>Made with 🐸</Text>
+          </Row>
+        </Rows>
       ),
+      action: "/goddog",
       intents: [
         <Button value="previous">⏪</Button>,
-        <Button value="banana">Banana</Button>,
+        <Button.Link href="https://t.me/+eLttcj65I-1jNjRh">
+          Socials
+        </Button.Link>,
+        <Button.Link href="https://interchain.axelar.dev/base/0xDDf7d080C82b8048BAAe54e376a3406572429b4e">
+          Interchain
+        </Button.Link>,
         <Button value="next">⏩</Button>,
       ],
     });
-  } else if (buttonValue === "previous") {
+  })
+  .frame("/goddog", (c) => {
+    const { buttonValue } = c;
+    console.log(buttonValue);
+    if (buttonValue === "next") {
+      return c.res({
+        image: (
+          <Rows backgroundColor="background" padding="32">
+            <Row height="3/4" alignVertical="center">
+              <Text color="text200" size="24">
+                GodDog ($OOOooo) is an omnichain cryptocurrency, offering
+                enhanced scalability, interoperability, and resilience across
+                multiple blockchains, ensuring broader accessibility and a more
+                robust decentralized ecosystem.
+              </Text>
+            </Row>
+            <Row
+              height="0.5/4"
+              border={"gray"}
+              borderBottom={"none"}
+              borderRight={"none"}
+              borderLeft={"none"}
+              alignVertical="bottom"
+            >
+              <Text size={"15"}>GodDog 🐕</Text>
+            </Row>
+          </Rows>
+        ),
+        action: "/goddog/1",
+        intents: [
+          <Button value="previous">⏪</Button>,
+          <Button.Link href="https://t.me/+eLttcj65I-1jNjRh">
+            Socials
+          </Button.Link>,
+          <Button.Link href="https://interchain.axelar.dev/base/0xDDf7d080C82b8048BAAe54e376a3406572429b4e">
+            Interchain
+          </Button.Link>,
+          <Button value="next">⏩</Button>,
+        ],
+      });
+    } else if (buttonValue === "previous") {
+      c.res({
+        action: "/",
+      });
+    }
     return c.res({
       image: (
-        <div
-          style={{
-            color: "white",
-            display: "flex",
-            fontSize: 40,
-          }}
-        >
-          {`
-        
-            GodDog's integration with an omnichain infrastructure ensures interoperability across multiple blockchain networks, enhancing accessibility and utility for users. This approach also bolsters security, scalability, and future adaptability, positioning GodDog as a robust and versatile memecoin within the broader blockchain ecosystem.
-        `}
-        </div>
+        <Rows backgroundColor={"background"}>
+          <Row alignVertical="center">
+            <Text>Unkown Error: 404 </Text>
+          </Row>
+        </Rows>
       ),
+      intents: [<Button.Reset>Reset</Button.Reset>],
+    });
+  })
+  .frame("goddog/1", (c) => {
+    const { buttonValue } = c;
+    if (buttonValue === "next") {
+      return c.res({
+        image: (
+          <Rows backgroundColor="background" padding="32">
+            <Row height="3/4" alignVertical="center">
+              <Text color="text200" size="18">
+                When Hermes DeFi Token V2 launches, it will introduce unified
+                liquidity, streamlining access to liquidity pools across various
+                decentralized finance protocols. This integration ensures
+                seamless interoperability and maximizes efficiency for users,
+                fostering a more fluid and interconnected DeFi ecosystem.
+              </Text>
+            </Row>
+            <Row
+              height="0.5/4"
+              border={"gray"}
+              borderBottom={"none"}
+              borderRight={"none"}
+              borderLeft={"none"}
+              alignVertical="bottom"
+            >
+              <Text size={"15"}>GodDog 🐕</Text>
+            </Row>
+          </Rows>
+        ),
+        action: "/",
+        intents: [
+          <Button value="previous">⏪</Button>,
+          <Button.Link href="https://t.me/+eLttcj65I-1jNjRh">
+            Socials
+          </Button.Link>,
+          <Button.Link href="https://interchain.axelar.dev/base/0xDDf7d080C82b8048BAAe54e376a3406572429b4e">
+            Interchain
+          </Button.Link>,
+          <Button value="next">⏩</Button>,
+        ],
+      });
+    } else if (buttonValue === "previous") {
+      return c.res({
+        action: "/goddog",
+      });
+    }
+
+    return c.res({
+      image: (
+        <Rows backgroundColor="background" padding="32">
+          <Row height="3/4" alignVertical="center">
+            <Text color="text200" size="18">
+              ERROR: 404
+            </Text>
+          </Row>
+          <Row
+            height="0.5/4"
+            border={"gray"}
+            borderBottom={"none"}
+            borderRight={"none"}
+            borderLeft={"none"}
+            alignVertical="bottom"
+          >
+            <Text size={"15"}>GodDog 🐕</Text>
+          </Row>
+        </Rows>
+      ),
+      action: "/",
       intents: [
         <Button value="previous">⏪</Button>,
-        <Button value="banana">Banana</Button>,
+        <Button.Link href="https://t.me/+eLttcj65I-1jNjRh">
+          Socials
+        </Button.Link>,
+        <Button.Link href="https://interchain.axelar.dev/base/0xDDf7d080C82b8048BAAe54e376a3406572429b4e">
+          Interchain
+        </Button.Link>,
         <Button value="next">⏩</Button>,
       ],
     });
-  }
-  return c.res({
-    image: (
-      <div
-        style={{
-          color: "white",
-          display: "flex",
-          fontSize: 60,
-          justifyContent: "center",
-        }}
-      >
-        {"GodDog: The future of memecoins"}
-      </div>
-    ),
-    intents: [
-      <Button value="previous">⏪</Button>,
-      <Button value="banana">Banana</Button>,
-      <Button value="next">⏩</Button>,
-    ],
   });
-});
+
+devtools(app, { serveStatic });
 
 serve({
   fetch: app.fetch,
   port: 3000,
 });
-console.log(`Server is running on port `);
